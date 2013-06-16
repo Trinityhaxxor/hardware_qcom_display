e80c65a8f7666c8bf54a08311fae7ad8ef51b216
Philippe Larrieux <philippe.larrieux@gmail.com>
2013-06-16 15:03:12 +0200

    bulk add Google last update
    Change-Id: I7c5777f606f035a70d7c3ca2988eb671a5c70574

--- libgenlock/Android.mk
+++ libgenlock/Android.mk
@@ -7,9 +7,6 @@ LOCAL_MODULE_TAGS             := optional
 LOCAL_C_INCLUDES              := $(common_includes)
 LOCAL_SHARED_LIBRARIES        := liblog libcutils
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"libgenlock\"
-ifneq ($(TARGET_BOARD_PLATFORM), msm8960)
-	LOCAL_CFLAGS += -DUSE_GENLOCK
-endif
 ifneq ($(TARGET_BOARD_PLATFORM), msm8660)
 	LOCAL_CFLAGS += -DUSE_GENLOCK
 endif
